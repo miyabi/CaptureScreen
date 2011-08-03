5a5a563db5e5cad83c5f864444b04b0692c72e07
Masayuki Iwai <miyabi@mybdesign.com>
2011-08-04 03:37:40 +0900

    First commit

---
+++ CaptureScreen/CaptureScreen.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Resources;
+using System.IO;
+using System.IO.IsolatedStorage;
+using Microsoft.Phone.Controls;
+using Microsoft.Xna.Framework.Media;
+
+
+namespace CaptureScreen
+{
+	public class CaptureScreen
+	{
+		static public Boolean capture(int quality)
+		{
+			try
+			{
+				PhoneApplicationFrame frame = (PhoneApplicationFrame)Application.Current.RootVisual;
+				WriteableBitmap bitmap = new WriteableBitmap((int)frame.ActualWidth, (int)frame.ActualHeight);
+				bitmap.Render(frame, null);
+				bitmap.Invalidate();
+
+				string fileName = DateTime.Now.ToString("'Capture'yyyyMMddHHmmssfff'.jpg'");
+				IsolatedStorageFile storage = IsolatedStorageFile.GetUserStoreForApplication();
+				if (storage.FileExists(fileName))
+					return false;
+
+				IsolatedStorageFileStream stream = storage.CreateFile(fileName);
+				bitmap.SaveJpeg(stream, bitmap.PixelWidth, bitmap.PixelHeight, 0, quality);
+				stream.Close();
+
+				stream = storage.OpenFile(fileName, FileMode.Open, FileAccess.Read);
+				MediaLibrary mediaLibrary = new MediaLibrary();
+				Picture picture = mediaLibrary.SavePicture(fileName, stream);
+				stream.Close();
+
+				storage.DeleteFile(fileName);
+			}
+			catch (Exception e)
+			{
+				MessageBox.Show(e.Message);
+				return false;
+			}
+
+			return true;
+		}
+	}
+}
---
+++ CaptureTest/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using Microsoft.Devices;
+using CaptureScreen;
+
+using System.Diagnostics;
+using Microsoft.Xna.Framework;
+using System.Windows.Media.Imaging;
+
+
+namespace CaptureTest
+{
+	public partial class MainPage : PhoneApplicationPage
+	{
+		// Constructor
+		public MainPage()
+		{
+			InitializeComponent();
+
+			captureButton.Click += new RoutedEventHandler(captureButton_Click);
+		}
+
+		void captureButton_Click(object sender, RoutedEventArgs e)
+		{
+			captureButton.Visibility = Visibility.Collapsed;
+			CaptureScreen.CaptureScreen.capture(100);
+			captureButton.Visibility = Visibility.Visible;
+		}
+	}
+}
